bbb2a26c1d779d854f9089739eee6be92b5cfa61
akulov1 <144591371+akulov1@users.noreply.github.com>
2024-04-22 10:13:30 +0300

    Add files via upload

---
+++ back4/db.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Applications (     
+    id INT AUTO_INCREMENT PRIMARY KEY,     
+    fio VARCHAR(255),     
+    year INT,     
+    email VARCHAR(255),   
+    gender ENUM('male', 'female'),  
+    biography TEXT,     
+    checkcontract TINYINT(1) );
+
+-- Create the Languages table
+CREATE TABLE programming_language (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) UNIQUE);
+-- Create the FormLanguages table to establish a many-to-many relationship between Forms and Languages
+CREATE TABLE application_language (     
+    application_id INT,     
+    language_id INT,     
+    FOREIGN KEY (application_id) REFERENCES Applications(id),   
+    FOREIGN KEY (language_id) REFERENCES programming_language(id),     
+    PRIMARY KEY (application_id, language_id) );
